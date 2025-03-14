5e7113a03c227d823b93cd6c2bb55f2dac0120d8
Victor Rodrigues <153961026+Victorlcr@users.noreply.github.com>
2025-03-13 21:19:54 -0300

    Create select_from_aula1.sql

---
+++ aula1/select_from_aula1.sql
@@ -0,0 +1,7 @@
+''' Nas consultas em Banco de Dados usamos letras maíusculas para palavras
+reservadas da linguaguem, assim, a declaração pode ser visualizada mais facilmente '''
+
+# Seleciona todas colunas e linhas da tabela Clientes
+SELECT * FROM Clientes
+
+SELECT Nome, Cargo, Endereco FROM Clientes
