a8d250961b8879fa615827177972f70253108dfb
Victor Rodrigues <153961026+Victorlcr@users.noreply.github.com>
2025-03-14 06:07:32 -0300

    Atualizar o select_from_aula1.sql

--- aula1/select_from_aula1.sql
+++ aula1/select_from_aula1.sql
@@ -2,18 +2,36 @@
 reservadas da linguaguem (SELECT, FROM, WHERE, ETC...), assim, a declaração pode ser visualizada mais facilmente */
 
 
-SELECT * FROM Clientes  -- Seleciona todas as colunas e linhas da tabela Clientes
-SELECT * FROM Pedidos -- Seleciona todas as colunas e linhas da tabela Pedidos
+-- Seleciona todas as colunas e linhas da tabela Clientes
 
-SELECT Nome, Cargo, Endereco FROM Clientes -- Seleciona as colunas Nome, Cargo e Endereco da tabela Clientes
+SELECT * FROM Clientes
 
-SELECT NumPed, Frete FROM Pedidos -- Seleciona as colunas NumPed e Frete da tabela Pedidos
+
+-- Seleciona todas as colunas e linhas da tabela Pedidos
+
+SELECT * FROM Pedidos
+
+
+-- Seleciona as colunas Nome, Cargo e Endereco da tabela Clientes
+
+SELECT Nome, Cargo, Endereco FROM Clientes
+
+
+-- Seleciona as colunas NumPed e Frete da tabela Pedidos
+
+SELECT NumPed, Frete FROM Pedidos
+
+
+-- Seleciona as colunas Nome, Pais da tabela Clientes onde a coluna Pais for igual a Brasil
 
 SELECT Nome, Pais FROM Clientes
-WHERE Pais = 'Brasil' -- Seleciona as colunas Nome, Pais da tabela Clientes onde a coluna Pais for igual a Brasil
+WHERE Pais = 'Brasil'
 
--- Exercício 1: Selecione Nome, Endereco, Cidade e Pais dos clientes do Canadá.
 
--- Exercício 2: Selecione todos os dados do pedido de NumPed igual a 10520.
+/* Exercícios para praticar */
 
--- Exercício 3: Crie uma consulta da sua escolha utilizando uma das duas tabelas apresentadas.
+-- 1: Selecione Nome, Endereco, Cidade e Pais dos clientes do Canadá.
+
+-- 2: Selecione todos os dados do pedido de NumPed igual a 10520.
+
+-- 3: Crie uma consulta da sua escolha utilizando uma das duas tabelas apresentadas.
